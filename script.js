5603a391e3b3a8ba47943191e21a594bd03a9f4e
Mahfouz <mafz060706@gmail.com>
2024-03-25 15:07:37 +0100

    Gameover completed

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ function Players(name,marker) {
 
 (function () {
     let box = document.querySelectorAll('.box')
+    let boxOne = document.querySelector('#box1')
+    let boxTwo = document.querySelector('#box2')
+    let boxThree = document.querySelector('#box3')
+    let boxFour = document.querySelector('#box4')
+    let boxFive = document.querySelector('#box5')
+    let boxSix = document.querySelector('#box6')
+    let boxSeven = document.querySelector('#box7')
+    let boxEight = document.querySelector('#box8')
+    let boxNine = document.querySelector('#box9')
     let gameOver = document.querySelector('dialog')
+    let winLossDraw = document.querySelector('#win-loss-draw')
 
     let gameBoard = {
         firstRow:  [,,],
@@ -20,8 +30,177 @@ function Players(name,marker) {
          player2 :  Players('Alice','O'),
          roundCount : 1,
          game : function () {
-            if (this.roundCount > 9) {
-                gameOver.showModal()
+            if (boxOne.textContent == 'X' && 
+            boxTwo.textContent == 'X' &&
+            boxThree.textContent == 'X') {
+            boxOne.setAttribute('style','background-color: #90ee90;')
+            boxTwo.setAttribute('style','background-color: #90ee90;')
+            boxThree.setAttribute('style','background-color: #90ee90;')
+        } else if (boxFour.textContent == 'X' && 
+            boxFive.textContent == 'X' &&
+            boxSix.textContent == 'X') {
+            boxFour.setAttribute('style','background-color: #90ee90;')
+            boxFive.setAttribute('style','background-color: #90ee90;')
+            boxSix.setAttribute('style','background-color: #90ee90;')
+        } else if (boxSeven.textContent == 'X' && 
+            boxEight.textContent == 'X' &&
+            boxNine.textContent == 'X') {
+            boxSeven.setAttribute('style','background-color: #90ee90;')
+            boxEight.setAttribute('style','background-color: #90ee90;')
+            boxNine.setAttribute('style','background-color: #90ee90;')
+        } else if (boxOne.textContent == 'X' && 
+            boxFive.textContent == 'X' &&
+            boxNine.textContent == 'X') {
+            boxOne.setAttribute('style','background-color: #90ee90;')
+            boxFive.setAttribute('style','background-color: #90ee90;')
+            boxNine.setAttribute('style','background-color: #90ee90;')
+        } else if (boxThree.textContent == 'X' && 
+            boxFive.textContent == 'X' &&
+            boxSeven.textContent == 'X') {
+            boxThree.setAttribute('style','background-color: #90ee90;')
+            boxFive.setAttribute('style','background-color: #90ee90;')
+            boxSeven.setAttribute('style','background-color: #90ee90;')
+        } else if (boxOne.textContent == 'X' && 
+            boxFour.textContent == 'X' &&
+            boxSeven.textContent == 'X') {
+            boxOne.setAttribute('style','background-color: #90ee90;')
+            boxFour.setAttribute('style','background-color: #90ee90;')
+            boxSeven.setAttribute('style','background-color: #90ee90;')
+        } else if (boxTwo.textContent == 'X' && 
+            boxFive.textContent == 'X' &&
+            boxEight.textContent == 'X') {
+            boxTwo.setAttribute('style','background-color: #90ee90;')
+            boxFive.setAttribute('style','background-color: #90ee90;')
+            boxEight.setAttribute('style','background-color: #90ee90;')
+        } else if (boxThree.textContent == 'X' && 
+            boxSix.textContent == 'X' &&
+            boxNine.textContent == 'X') {
+            boxThree.setAttribute('style','background-color: #90ee90;')
+            boxSix.setAttribute('style','background-color: #90ee90;')
+            boxNine.setAttribute('style','background-color: #90ee90;')
+        } else if (boxOne.textContent == 'O' && 
+            boxTwo.textContent == 'O' &&
+            boxThree.textContent == 'O') {
+            boxOne.setAttribute('style','background-color: #90ee90;')
+            boxTwo.setAttribute('style','background-color: #90ee90;')
+            boxThree.setAttribute('style','background-color: #90ee90;')
+        } else if (boxFour.textContent == 'O' && 
+            boxFive.textContent == 'O' &&
+            boxSix.textContent == 'O') {
+            boxFour.setAttribute('style','background-color: #90ee90;')
+            boxFive.setAttribute('style','background-color: #90ee90;')
+            boxSix.setAttribute('style','background-color: #90ee90;')
+        } else if (boxSeven.textContent == 'O' && 
+            boxEight.textContent == 'O' &&
+            boxNine.textContent == 'O') {
+            boxSeven.setAttribute('style','background-color: #90ee90;')
+            boxEight.setAttribute('style','background-color: #90ee90;')
+            boxNine.setAttribute('style','background-color: #90ee90;')
+        } else if (boxOne.textContent == 'O' && 
+            boxFive.textContent == 'O' &&
+            boxNine.textContent == 'O') {
+            boxOne.setAttribute('style','background-color: #90ee90;')
+            boxFive.setAttribute('style','background-color: #90ee90;')
+            boxNine.setAttribute('style','background-color: #90ee90;')
+        } else if (boxThree.textContent == 'O' && 
+            boxFive.textContent == 'O' &&
+            boxSeven.textContent == 'O') {
+            boxThree.setAttribute('style','background-color: #90ee90;')
+            boxFive.setAttribute('style','background-color: #90ee90;')
+            boxSeven.setAttribute('style','background-color: #90ee90;')
+        } else if (boxOne.textContent == 'O' && 
+            boxFour.textContent == 'O' &&
+            boxSeven.textContent == 'O') {
+            boxOne.setAttribute('style','background-color: #90ee90;')
+            boxFour.setAttribute('style','background-color: #90ee90;')
+            boxSeven.setAttribute('style','background-color: #90ee90;')
+        } else if (boxTwo.textContent == 'O' && 
+            boxFive.textContent == 'O' &&
+            boxEight.textContent == 'O') {
+            boxTwo.setAttribute('style','background-color: #90ee90;')
+            boxFive.setAttribute('style','background-color: #90ee90;')
+            boxEight.setAttribute('style','background-color: #90ee90;')
+        } else if (boxThree.textContent == 'O' && 
+            boxSix.textContent == 'O' &&
+            boxNine.textContent == 'O') {
+            boxThree.setAttribute('style','background-color: #90ee90;')
+            boxSix.setAttribute('style','background-color: #90ee90;')
+            boxNine.setAttribute('style','background-color: #90ee90;')
+        } 
+
+
+
+
+            if (this.roundCount <= 10) {
+                if ((boxOne.textContent == 'X' && 
+                    boxTwo.textContent == 'X' &&
+                    boxThree.textContent == 'X')
+                    ||
+                    (boxFour.textContent == 'X' &&
+                    boxFive.textContent == 'X' &&
+                    boxSix.textContent == 'X') 
+                    ||
+                    (boxSeven.textContent == 'X' &&
+                    boxEight.textContent == 'X' &&
+                    boxNine.textContent == 'X')
+                    ||
+                    (boxOne.textContent == 'X' &&
+                    boxFive.textContent == 'X' &&
+                    boxNine.textContent == 'X')
+                    ||
+                    (boxThree.textContent == 'X' &&
+                    boxFive.textContent == 'X' &&
+                    boxSeven.textContent == 'X')
+                    ||
+                    (boxOne.textContent == 'X' &&
+                    boxFour.textContent == 'X' &&
+                    boxSeven.textContent == 'X')
+                    ||
+                    (boxTwo.textContent == 'X' &&
+                    boxFive.textContent == 'X' &&
+                    boxEight.textContent == 'X')
+                    ||
+                    (boxThree.textContent == 'X' &&
+                    boxSix.textContent == 'X' &&
+                    boxNine.textContent == 'X')) {
+                    winLossDraw.textContent = `X wins!`
+                    gameOver.showModal()
+                } else if ((boxOne.textContent == 'O' && 
+                            boxTwo.textContent == 'O' &&
+                            boxThree.textContent == 'O')
+                            ||
+                            (boxFour.textContent == 'O' &&
+                            boxFive.textContent == 'O' &&
+                            boxSix.textContent == 'O') 
+                            ||
+                            (boxSeven.textContent == 'O' &&
+                            boxEight.textContent == 'O' &&
+                            boxNine.textContent == 'O')
+                            ||
+                            (boxOne.textContent == 'O' &&
+                            boxFive.textContent == 'O' &&
+                            boxNine.textContent == 'O')
+                            ||
+                            (boxThree.textContent == 'O' &&
+                            boxFive.textContent == 'O' &&
+                            boxSeven.textContent == 'O')
+                            ||
+                            (boxOne.textContent == 'O' &&
+                            boxFour.textContent == 'O' &&
+                            boxSeven.textContent == 'O')
+                            ||
+                            (boxTwo.textContent == 'O' &&
+                            boxFive.textContent == 'O' &&
+                            boxEight.textContent == 'O')
+                            ||
+                            (boxThree.textContent == 'O' &&
+                            boxSix.textContent == 'O' &&
+                            boxNine.textContent == 'O')) {
+                    winLossDraw.textContent = `O wins!`
+                    gameOver.showModal()
+                } else {
+                    winLossDraw.textContent = `It's a tie`
+                }
             }
          }
     }
@@ -29,6 +208,7 @@ function Players(name,marker) {
     let display = {
         clickBox : function () {
             box.forEach(element => element.addEventListener('click', () => {
+                gameFlow.game()
                 if (gameFlow.roundCount % 2 == 0) {
                     element.textContent = `${gameFlow.player2.playerMarker}`
                     element.setAttribute('style','color: #0000ff;')
@@ -38,7 +218,6 @@ function Players(name,marker) {
                 }
 
                 ++gameFlow.roundCount
-                gameFlow.game()
 
                 if (element.textContent == 'X') {
                     element.setAttribute('style','pointer-events: none; color: #ffa500;')
